79b931dc7471f7d2a7e4e184367299b24ba1aa1f
Himesh Suthar <himeshjangid0000@gmail.com>
2025-03-22 18:44:22 +0530

    Add cart store with Zustand for managing shopping cart functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -58,7 +58,3 @@ export async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: '/api/:path*',
-};
---
+++ src/stores/cartStore.ts
@@ -0,0 +1,84 @@
+import Product from '@/types/product';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface CartItem extends Product {
+  quantity: number;
+  totalPrice: number;
+}
+
+interface CartStore {
+  cart: CartItem[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (product: Product) => void;
+  clearCart: () => void;
+  returnTotalPrice: () => number;
+}
+
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+      addToCart: (product) => {
+        set((state) => {
+          const existingProduct = state.cart.find((item) => item.id === product.id);
+          if (existingProduct) {
+            return {
+              cart: state.cart.map((item) =>
+                item.id === product.id
+                  ? { ...item, quantity: item.quantity + 1, totalPrice: (item.quantity + 1) * item.price }
+                  : item
+              ),
+            };
+          } else {
+            return {
+              cart: [...state.cart, { ...product, quantity: 1, totalPrice: product.price }],
+            };
+          }
+        });
+      },
+      removeFromCart: (product) => {
+        set((state) => {
+          const existingProduct = state.cart.find((item) => item.id === product.id);
+          if (!existingProduct) return state;
+
+          if (existingProduct.quantity > 1) {
+            return {
+              cart: state.cart.map((item) =>
+                item.id === product.id
+                  ? { ...item, quantity: item.quantity - 1, totalPrice: (item.quantity - 1) * item.price }
+                  : item
+              ),
+            };
+          } else {
+            return {
+              cart: state.cart.filter((item) => item.id !== product.id),
+            };
+          }
+        });
+      },
+      clearCart: () => {
+        set({ cart: [] });
+      },
+      returnTotalPrice: () => {
+        return get().cart.reduce((acc, item) => acc + item.totalPrice, 0);
+      },
+    }),
+    {
+      name: 'cart-storage',
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      onRehydrateStorage: (state) => {
+        console.log('hydration starts');
+
+        // optional
+        return (state, error) => {
+          if (error) {
+            console.log('an error happened during hydration', error);
+          } else {
+            console.log('hydration finished');
+          }
+        };
+      },
+    }
+  )
+);
