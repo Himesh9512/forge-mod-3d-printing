212db6c03b8f93c712d1a08e865351412d993d62
Himesh Suthar <himeshjangid0000@gmail.com>
2025-03-26 23:56:48 +0530

    Refactor MongoDB connection logic to cache connections and improve performance

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,10 +4,27 @@ const MONGODB_URI = process.env.MONGODB_URI || '';
 
 if (!MONGODB_URI) throw new Error('MongoDB URI is missing!');
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const cached = (global as any).mongoose || { conn: null, promise: null };
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
-    console.log('✅ MongoDB Connected');
+    if (cached.conn) return cached.conn;
+
+    if (!cached.promise) {
+      cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+        console.log('MongoDB Connected with Mongoose');
+        return mongoose;
+      });
+    }
+
+    cached.conn = await cached.promise;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (global as any).mongoose = cached; // ✅ Store connection globally
+    return cached.conn;
+
+    // await mongoose.connect(MONGODB_URI);
+    // console.log('✅ MongoDB Connected');
   } catch (error) {
     console.error('❌ MongoDB Connection Error:', error);
     process.exit(1);
